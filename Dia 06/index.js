c1d1207a5eb80d333c628d636c61ca655f8b5e3f
Bernardo Bilio <bernardobilio@gmail.com>
2025-01-12 13:27:00 -0300

    arquivos do dia 06

---
+++ Dia 06/index.js
@@ -0,0 +1,21 @@
+//    #1
+// CRIANDO VARIAVEIS 
+let numero = parseInt(prompt("Digte um numero inteiro positivo"))
+
+//CRIANDO LOOP FOR
+for (let i = 0; i <= numero; i++) {
+    console.log(i)
+}
+
+
+// #2 
+// CRIANDO UM LOOP FOR
+for (let i = 0; i <= 50; i += 5) {
+    console.log(i)
+}
+
+// #3
+// CRIANDO LOOP FOR
+for (let i = 50; i >= 0; i -= 5) {
+    console.log(i)
+}
